9f4784c85505e30f24f4c6a350da095ef704f10f
jenijaaaaad2 <jenny_dignom@yahoo.com>
2021-02-07 18:40:34 +0800

    Single Page Applications Part 2

--- confusion/src/components/DishDetailComponent.js
+++ confusion/src/components/DishDetailComponent.js
@@ -1,10 +1,10 @@
-import React from 'react'
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap'
-
+import React from 'react';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap'
+import { Link } from 'react-router-dom';
 
 function RenderDish({dish}) {
-		if(dish != null){
-			return (
+    if(dish != null){
+        return (
                 <div className = 'col-12 col-md-5 m-1'>
                     <Card>
                         <CardImg width="100%" src={dish.image} alt={dish.name}/>
@@ -14,40 +14,58 @@ function RenderDish({dish}) {
                         </CardBody>
                     </Card>
                 </div>
-            )
-		}
-	}
-
-function RenderComments({comments}) {
-		if(comments!= null){
-			comments.map((comment, i) => {
-                return (
-                    <ul className ='list-unstyled' key = {comment.id}>
-                        <li>
-                            <div>{comment.comment}</div>
-                            <div>{`-- ${comment.author} , ${new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day:'2-digit'}).format(new Date(Date.parse(comment.date)))}`}</div>
-                        </li>
-                    </ul>
-                )
-            })	
-        }
-	}
-
-const DishDetail = (props) => {
-    const dish = props.selectedDish
-    if(dish === null || dish === undefined) return (<div></div>)
-
-    return (
-        <div className ='container' >
-            <div className = 'row'>
-                <RenderDish dish = {dish} />
-                <div className = 'col-12 col-md-5 m-1'>
-                <h4>Comments</h4>	
-                    <RenderComments comments = {dish.comments} />
-                </div>
-            </div>
-        </div>
-    )
+        )
+    }
 }
 
-export default DishDetail  
+function RenderComments({comments}) {
+	if(comments!= null){
+        return (		
+            <div className = 'col-12 col-md-5 m-1'>
+            {
+                comments.map((comment, i) => {
+                    return (
+                        <ul className ='list-unstyled' key = {comment.id}>
+                            <li>
+                                <div>{comment.comment}</div>
+                                <div>{`-- ${comment.author} , ${new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day:'2-digit'}).format(new Date(Date.parse(comment.date)))}`}</div>
+                            </li>
+                        </ul>
+                    )
+                })					
+            }
+            </div>
+        )
+    }
+}
+
+const DishDetail = (props) => {
+		const dish = props.dish
+		const comments = props.comments
+		if(dish === null || dish === undefined) return (<div></div>)
+
+		return (
+			<div className ='container' >
+				<div className = 'row'>
+					<Breadcrumb>
+						<BreadcrumbItem>
+							<Link to='/home'>Home</Link>
+						</BreadcrumbItem>
+						<BreadcrumbItem>
+							<Link to='/menu'>Menu</Link>
+						</BreadcrumbItem>						
+						<BreadcrumbItem active>
+							{dish.name}
+						</BreadcrumbItem>
+					</Breadcrumb>
+			    </div>
+			    <div className = 'container'>
+			    	<div className = 'row'>
+						<RenderDish dish = {dish} />
+						<RenderComments comments = {comments} />			    	
+			    	</div>
+				</div>
+			</div>
+		)
+	}
+export default DishDetail 
--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -1,37 +1,42 @@
 import React, { Component } from 'react';
 import Menu from './MenuComponent';
-
 import DishDetail from './DishDetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import Contact from './ContactComponent';
 import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
-import { PROMOTIONS } from '../shared/promotion';
 import { LEADERS } from '../shared/leaders';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import { PROMOTIONS } from '../shared/promotions';
 
 class Main extends Component {
-
   constructor(props) {
     super(props)
-
     this.state = {
-      dishes: DISHES,
+      dishes: DISHES, 
       comments: COMMENTS,
-      promotions: PROMOTIONS,
-      leaders: LEADERS 
+      leaders: LEADERS,
+      promotions: PROMOTIONS
     }
   }
-
   render() {
     const HomePage = () => {
       return (
         <Home 
-            dish={this.state.dishes.filter((dish) => dish.featured)[0]}
-            promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
-            leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+          dish = {this.state.dishes.filter((dish)=> dish.featured)[0] } 
+          promotion = {this.state.promotions.filter((promotion) => promotion.featured)[0]}
+          leader = {this.state.leaders.filter((leader) => leader.featured)[0]}
+        />
+      )
+    }
+
+    const DishWithId = ({match}) => {
+      return(
+        <DishDetail 
+          dish = {this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId))[0]} 
+          comments = {this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId))}
         />
       )
     }
@@ -42,7 +47,8 @@ class Main extends Component {
         <Switch>
           <Route path = "/home" component = { HomePage } />
           <Route exact path = "/menu" component = { () => <Menu dishes = {this.state.dishes} /> } />
-          <Route exact path = "/contactus" component = {Contact} />
+          <Route path = "/menu/:dishId" component = { DishWithId } />
+          <Route exact path = "/contactus" component = { Contact } />
           <Redirect to = '/home' />
         </Switch>
         <Footer/>
